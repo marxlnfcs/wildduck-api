995f0308df632ef323f3dc9b90ba518473035171
Marx <marxlnfcs@gmail.com>
2023-05-04 18:41:36 +0200

    Updated packages

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -17,19 +17,38 @@ delete packageJsonContent['directories'];
 
 // remove all devDependencies except "@types/*"
 const dependencies = Object.keys(packageJsonContent.dependencies || {});
-const devDependencies = Object.keys(packageJsonContent.devDependencies || {});
-for(let devDependency of devDependencies){
-    if(!devDependency.startsWith('@types/')) {
-        delete packageJsonContent.devDependencies[devDependency];
-        continue;
-    }
-    if(devDependency === '@types/node'){
-        continue;
-    }
-    if(!dependencies.includes(devDependency.replace('@types/', ''))){
-        delete packageJsonContent.devDependencies[devDependency];
-    }
+const peerDependencies = Object.keys(packageJsonContent.peerDependencies || {});
+
+// remove all devDependencies that are not starting with @types/*
+if(packageJsonContent.devDependencies){
+    Object.keys(packageJsonContent.devDependencies).map(p => {
+        if(!p.startsWith('@types/')){
+            delete packageJsonContent.devDependencies[p];
+            return;
+        }
+        if(
+            !dependencies.includes(p.replace('@types/', '')) &&
+            !peerDependencies.includes(p.replace('@types/', ''))
+        ){
+            delete packageJsonContent.devDependencies[p];
+        }
+    });
 }
+
+// move all dependencies to peerDependencies
+if(packageJsonContent['dependencies']){
+    packageJsonContent['peerDependencies'] = packageJsonContent['peerDependencies'] || {};
+    Object.keys(packageJsonContent['dependencies']).map(p => {
+        if(!Object.keys(packageJsonContent['peerDependencies']).includes(p)){
+            packageJsonContent['peerDependencies'][p] = packageJsonContent['dependencies'][p];
+        }
+    });
+}
+
+// remove all unwanted properties
+delete packageJsonContent['scripts'];
+delete packageJsonContent['directories'];
+delete packageJsonContent['dependencies'];
 
 // save package.json
 fs.writeFileSync(packageJson, JSON.stringify(packageJsonContent, null, 3));
